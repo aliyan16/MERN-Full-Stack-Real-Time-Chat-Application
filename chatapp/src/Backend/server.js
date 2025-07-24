3f3f376cb951557183e753571f835a008dc9bc13
aliyan16 <aliyanm12376@gmail.com>
2025-07-24 17:16:12 +0500

    backend code for register accounts

--- chatapp/src/Backend/server.js
+++ chatapp/src/Backend/server.js
@@ -2,6 +2,8 @@ const mongoose=require('mongoose')
 const express=require('express')
 const bodyParser=require('body-parser')
 const cors=require('cors')
+const RegisterAccounts=require('./models/RegisterSchema')
+const bycrypt=require('bycrypt')
 
 
 const app=express()
@@ -12,3 +14,32 @@ mongoUrl='mongodb+srv://aliyanm12376:aliyan123@cluster0.kzpdbcw.mongodb.net/chat
 
 app.use(bodyParser.json())
 app.use(cors())
+
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on('open',()=>{
+    console.log('DB Connected')
+})
+
+
+app.post('/register',async(req,res)=>{
+    try{
+        const {firstName,lastName,email,password,dob,gender}=req.body
+        const user=await RegisterAccounts.findOne(email)
+        if(user){
+            return res.status(400).json({error:'User already exists'})
+        }
+        const saltRounds=15
+        const hashedPassword=await bycrypt.hash(password,saltRounds)
+        const newUser=new RegisterAccounts({firstName,lastName,email,hashedPassword,dob,gender})
+        await newUser.save()
+        res.status(201).json({message:'Account created successfully'})
+    }catch(e){
+        console.error('error creating account')
+        res.status(500).json({error:'Server error'})
+    }
+})
+
